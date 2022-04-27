e761adc1c34f4da894f7e8d0aaf2b6bc1aa099b3
Tomas alberto Pedrajas ferrer <tompedra@c2r15s4.42madrid.com>
2022-04-27 21:45:48 +0200

    printf changes V1

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,53 +6,66 @@
 /*   By: tompedra <tompedra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:30:32 by tompedra          #+#    #+#             */
-/*   Updated: 2022/04/25 23:38:54 by tompedra         ###   ########.fr       */
+/*   Updated: 2022/04/27 21:40:42 by tompedra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 static int	ft_process_param(char const format, va_list args)
 {
 	int	result;
 
 	result = 0;
-	if (format == 'c')
-
-	if (format == 's')
-
-	if (format == 'p')
-
-	if (format == 'd')
-
-	if (format == 'i')
-
-	if (format == 'u')
-
-	if (format == 'x')
-
-	if (format == 'X')
-
-	if (format == '%')
-		
+	/*if (format == 'c')
+		result = ft_print_char_param(va_arg(args, char))
+	else if (format == 's')
+		result = ft_print_str_param(va_arg(args, char *));
+	else if (format == 'p')
+		result = ft_print_ptr_param(va_arg(args, size_t))
+	else if (format == 'd' || format == 'i')
+		result = ft_print_int_param(va_arg(args, int));
+	else if (format == 'u')
+		result = ft_print_uint_param(va_arg(args, unsigned int));
+	else if (format == 'x' || format == 'X')
+		result = ft_print_hex_param(va_arg(args, unsigned int), format);
+	else if (format == '%')
+		result = ft_print_char_param(format);
+	else
+		result = -1;*/
 	return (result);
 }
 
 int	ft_printf(char const *format, ...)
 {
 	va_list	params;
+	char	**split_params;
 	int		result;
 
 	result = 0;
 	va_start(params, format);
 	while (*format)
 	{
+		param_len = ft_strchr(format, '%') - format;
+		result = 
+	}
+
+	while (*format)
+	{
 		if (*format == '%')
 		{
-			result += ft_process_param(format, params);
+			param_len = ft_process_param(*format, params);
+			if (param_len != -1)
+				result += param_len;
 		}
 		
 	}
 	va_end(params);
 	return (result);
+}
+
+int	main(void)
+{
+	printf("% hola %");
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tompedra <tompedra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:30:36 by tompedra          #+#    #+#             */
-/*   Updated: 2022/04/25 20:45:27 by tompedra         ###   ########.fr       */
+/*   Updated: 2022/04/27 20:46:04 by tompedra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 # include "./libft/libft.h"
 # include <stdarg.h>
+# include <unistd.h>
+
+int	ft_print_char_param(char param);
+int	ft_print_str_param(char *param);
+int	ft_print_ptr_param(size_t param);
+int	ft_print_int_param(int param);
+int	ft_print_uint_param(unsigned int param);
+int	ft_print_hex_param(unsigned int param);
 
 int	ft_printf(char const *format, ...);
 
