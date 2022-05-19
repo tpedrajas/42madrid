20b3bd7d604d473280064b7cb71880fdc0a57882
Tomas alberto Pedrajas ferrer <tompedra@c3r13s4.42madrid.com>
2022-05-19 23:49:21 +0200

    improve bonus printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,36 +6,13 @@
 /*   By: tompedra <tompedra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:30:32 by tompedra          #+#    #+#             */
-/*   Updated: 2022/05/13 22:40:58 by tompedra         ###   ########.fr       */
+/*   Updated: 2022/05/19 23:47:59 by tompedra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
-static int	ft_process_param_type(const char format, va_list args)
-{
-	int	result;
-
-	result = 0;
-	if (format == 'c')
-		result = ft_combine_str_param(&va_arg(args, char), format);
-	else if (format == 's')
-		result = ft_combine_str_param(va_arg(args, char *), format);
-	else if (format == 'p')
-		result = ft_combine_hex_param(va_arg(args, size_t), format);
-	else if (format == 'd' || format == 'i')
-		result = ft_combine_llong_param(va_arg(args, int));
-	else if (format == 'u')
-		result = ft_combine_llong_param(va_arg(args, unsigned int));
-	else if (format == 'x' || format == 'X')
-		result = ft_combine_hex_param(va_arg(args, unsigned int), format);
-	else if (format == '%')
-		result = ft_combine_str_param(&format, 'c');
-	else
-		result = -1;
-	return (result);
-}
+static t_mark **ft_get_marks(char const )
 
 int	ft_printf(char const *format, ...)
 {
@@ -58,9 +35,4 @@ int	ft_printf(char const *format, ...)
 	}
 	va_end(params);
 	return (result);
-}
-
-int	main(void)
-{
-	printf("%03d", 10);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,21 +6,32 @@
 /*   By: tompedra <tompedra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:30:36 by tompedra          #+#    #+#             */
-/*   Updated: 2022/05/13 22:00:32 by tompedra         ###   ########.fr       */
+/*   Updated: 2022/05/19 22:39:47 by tompedra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include "./libft/libft.h"
+# include "libft/libft.h"
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_combine_str_param(const char *param, const char format);
-int	ft_combine_llong_param(int param);
-int	ft_combine_hex_param(unsigned int param);
+typedef struct s_mark
+{
+	char			*data;
+	struct s_mark	*next;
+	char			justify;
+	int				fill;
+	char			fill_char;
+	int				precision;
+	int				sign;
+}	t_mark;
 
-int	ft_printf(char const *format, ...);
+int		ft_printf(char const *format, ...);
+t_mark	*ft_process_mark(const char *format, va_list args);
+char	*ft_get_str_datatype(const char *param, const char format);
+char	*ft_get_nbr_datatype(size_t param);
+char	*ft_get_hex_datatype(size_t param, const char format);
 
 #endif
